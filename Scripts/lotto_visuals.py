ddd17bac90488c2311d96a3e6929b39e34bdc336
Jared Dominic Caraan (DHL SG), external <JaredDominic.Caraan@dhl.com>
2020-04-30 04:10:14 +0800

    created new jupyter notebook

--- Scripts/lotto_visuals.py
+++ Scripts/lotto_visuals.py
@@ -3,13 +3,13 @@ import plotly.graph_objects as go
 
 trace0 = go.Scatter(
     x=[1, 2, 3, 4],
-    y=[10, 15, 13, 17]
+    y=[10, 15, 13, 20]
 )
 trace1 = go.Scatter(
     x=[1, 2, 3, 4],
-    y=[16, 5, 11, 9]
+    y=[16, 5, 11, 25]
 )
 
 data = [trace0, trace1]
 
-py.plot(data, filename = 'My First Graph', auto_open = True)
+py.plot(data, filename = 'My Second Graph', auto_open = True)
