c5951d31fb1e05e9d7aaf2e8e5b0ae49ab91c825
Jared Dominic V. Caraan <j.caraan04@gmail.com>
2020-07-22 00:33:51 +0800

    refactored get_combi.py; created visuals for whole dataset

--- Scripts/get_combination.py
+++ Scripts/get_combination.py
@@ -20,23 +20,20 @@ def get_combi(df):
     filt = df['Day_Name'] == next_day
     df_filtered = df[filt]
     
+    print("For " + next_day + " results")
     for i in num_col:
         
         num_list = list(df_filtered[i].unique())
-            
+        print(i)
         for j in num_list:
             
-            if i == 'first':
-                count = (((df_filtered[i].values == j).sum()) / len(df_filtered))          
-                probs[j] = count
-            else:
-                count = ((((df_filtered[i].values == j).sum()) / len(df_filtered)) * (last_prob))           
-                probs[j] = count
+            count = round((((df_filtered[i].values == j).sum()) / len(df_filtered)),2)
+            probs[j] = count
             
         num_high.append(max(probs.items(), key=operator.itemgetter(1))[0])
         last_prob = max(probs.items(), key=operator.itemgetter(1))[1]
         print(last_prob)
-        prob = {}
+        probs = {}
             
     return num_high
 
