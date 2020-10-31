05074793be84e2a9eb9dc523ca23ace8444c0444
Jared Dominic V. Caraan <j.caraan04@gmail.com>
2020-11-01 04:10:55 +0800

    exported 2006-2005 data to excel

--- Scripts/lotto_2005.py
+++ Scripts/lotto_2005.py
@@ -2,7 +2,49 @@ import pandas as pd
 
 from WebTable import WebTable
 from selenium import webdriver
-from config import chromedriver, second_batch
+from config import chromedriver, second_batch, filename_2005
+
+def date_name(x):
+    
+    return x[:x.index(',')]
+
+def separate(x):
+    
+    return x[x.index(',')+2:]
+    
+def odd_even(x):
+
+     pattern_str = list(x.split(","))
+     
+     pattern_list = []
+     res = "-"
+     
+     for i in pattern_str:
+        if int(i.lstrip('0')) % 2 == 0:
+            pattern_list.append("even")
+        else:
+            pattern_list.append("odd")
+     
+     return (res.join(pattern_list))
+     
+def odd_even_dist(x):
+    
+    pattern_str = list(x.split("-"))
+    
+    even_count = 0
+    odd_count = 0
+    
+    for i in pattern_str:
+        if i == 'even':
+            even_count += 1
+        else:
+            odd_count += 1
+            
+    return "Even: {}, Odd: {}".format(str(even_count), str(odd_count))
+
+def join(df):
+    
+    return df['first'] + ',' + df['second'] + ',' + df['third'] + ',' + df['fourth'] + ',' + df['fifth'] + ',' +  df['sixth']
 
 def main():
     
@@ -30,11 +72,12 @@ def main():
     #Implicitly wait for the page to load
     driver.implicitly_wait(30)
     
-    # button
+    # Go back to third to the last page
     driver.find_element_by_xpath('//table[@class="FDPagerLeft"]/tbody/tr/td[7]/img[@class="ccpoint"]').click()
     driver.find_element_by_xpath('//table[@class="FDPagerLeft"]/tbody/tr/td[2]/img[@class="ccpoint"]').click()
     driver.find_element_by_xpath('//table[@class="FDPagerLeft"]/tbody/tr/td[2]/img[@class="ccpoint"]').click()
     
+    # Load third to the last page
     date_table   = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[@class="restcdd"]')
     first_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[3]')
     second_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[4]')
@@ -45,35 +88,41 @@ def main():
     
     for p in range(len(date_table)):
         row_date.append(date_table[p].text)
-    
-    for p in range(1, len(first_table)-1):
-        row_first.append(first_table[p].text)
         row_second.append(second_table[p].text)
         row_third.append(third_table[p].text)
         row_fourth.append(fourth_table[p].text)
         row_fifth.append(fifth_table[p].text)
         row_sixth.append(sixth_table[p].text)
     
-    driver.find_element_by_xpath('//table[@class="FDPagerLeft"]/tbody/tr/td[8]/img[@class="ccpoint"]').click()
-    date_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[@class="restcdd"]')
-    first_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[3]')
-    second_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[4]')
-    third_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[5]')
-    fourth_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[6]')
-    fifth_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[7]')
-    sixth_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[8]')
-    
-    for p in range(len(date_table)):
-        row_date.append(date_table[p].text)
-    
-    for p in range(1, len(first_table)-1):
+    for p in range(1, len(first_table)):
         row_first.append(first_table[p].text)
-        row_second.append(second_table[p].text)
-        row_third.append(third_table[p].text)
-        row_fourth.append(fourth_table[p].text)
-        row_fifth.append(fifth_table[p].text)
-        row_sixth.append(sixth_table[p].text)
         
+    driver.implicitly_wait(10)
+    
+    # Load second to the last page
+    driver.find_element_by_xpath('//table[@class="FDPagerLeft"]/tbody/tr/td[8]/img[@class="ccpoint"]').click()
+    date_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[@class="restcdd"]')
+    first_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[3]')
+    second_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[4]')
+    third_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[5]')
+    fourth_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[6]')
+    fifth_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[7]')
+    sixth_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[8]')
+    
+    for p in range(len(date_table)): 
+        row_date.append(date_table[p].text)
+        row_second.append(second_table[p].text)
+        row_third.append(third_table[p].text)
+        row_fourth.append(fourth_table[p].text)
+        row_fifth.append(fifth_table[p].text)
+        row_sixth.append(sixth_table[p].text)
+    
+    for p in range(1, len(first_table)):
+        row_first.append(first_table[p].text)
+        
+    driver.implicitly_wait(10)
+    
+    # Load last page
     driver.find_element_by_xpath('//table[@class="FDPagerLeft"]/tbody/tr/td[8]/img[@class="ccpoint"]').click()
     date_table = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[@class="restcdd"]')
     first_table  = driver.find_elements_by_xpath('//table[@class="restbl"]/tbody/tr/td[3]')
@@ -85,22 +134,33 @@ def main():
     
     for p in range(len(date_table)):
         row_date.append(date_table[p].text)
-    
-    for p in range(1, len(first_table)-1):
-        row_first.append(first_table[p].text)
         row_second.append(second_table[p].text)
         row_third.append(third_table[p].text)
         row_fourth.append(fourth_table[p].text)
         row_fifth.append(fifth_table[p].text)
         row_sixth.append(sixth_table[p].text)
     
+    for p in range(1, len(first_table)):
+        row_first.append(first_table[p].text)
+        
+    driver.close()
+    
+    # Create dataframe
     df = pd.DataFrame(list(zip(row_date, row_first, row_second, row_third, row_fourth, row_fifth, row_sixth)), 
                         columns = ['Date', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth'])
     
-    print(df.head())
-    print(df.tail())
+    df['Day_Name'] = df['Date'].apply(lambda x: date_name(x))
+    df['Date']     = df['Date'].apply(lambda x: separate(x))
+    df['Date']     = pd.to_datetime(df['Date'], format = '%B %d, %Y')
+    
+    df['Winning Numbers'] = df.apply(join, axis = 1)
+    df['Odd_Even']        = df['Winning Numbers'].apply(lambda x: odd_even(x))
+    df['Odd_Even_Dist']   = df['Odd_Even'].apply(lambda x: odd_even_dist(str(x)))
+    
+    df = df[['Date', 'Winning Numbers', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'Day_Name', 'Odd_Even', 'Odd_Even_Dist']]
+    
+    # Export to excel
+    df.to_excel(filename_2005)
  
-    driver.close()
-
 if __name__ == "__main__":
     main()
