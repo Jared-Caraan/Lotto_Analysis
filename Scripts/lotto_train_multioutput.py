b11bd1979e1ee86387b134303e1c0a35e61b756a
Jared Dominic V. Caraan <j.caraan04@gmail.com>
2020-11-12 00:50:43 +0800

    tried some classifiers

--- Scripts/lotto_train_multioutput.py
+++ Scripts/lotto_train_multioutput.py
@@ -6,7 +6,9 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.neighbors import RadiusNeighborsClassifier, KNeighborsClassifier
 from config import *
 
 def main():
@@ -15,14 +17,17 @@ def main():
     df = pd.read_excel(filename_all)
     
     # Transformation
-    df['Date']      = pd.to_datetime(df['Date'], format = '%d/%m/%Y')
-    df['Day_Num']   = df['Date'].dt.weekday
-    df['Year']      = df['Date'].dt.year
+    df['Date']  = pd.to_datetime(df['Date'], format = '%d/%m/%Y')
+    df['Week']  = df['Date'].dt.isocalendar().week
+    df['Month'] = df['Date'].dt.month
     
-    df = df[['Day_Num', 'Year', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth']]
+    df = df[['Week', 'Month', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth']]
     
-    factor_year = pd.factorize(df['Year'])
-    df.Year     = factor_year[0]
+    factor_week = pd.factorize(df['Week'])
+    df.Week     = factor_week[0]
+    
+    factor_month = pd.factorize(df['Month'])
+    df.Month     = factor_month[0]
     
     # Split into dependent and independent variables
     X = df.iloc[:,0:2].values 
@@ -37,18 +42,20 @@ def main():
     X_test  = scaler.transform(X_test)
     
     # Model
-    d_tree = DecisionTreeClassifier(random_state=0)
+    d_tree = ExtraTreesClassifier()
     classifier = MultiOutputClassifier(d_tree)
     classifier.fit(X_train, y_train)
     
     # Score
     y_pred = classifier.predict(X_test)
-    #print(classifier.score(X_test, y_pred))
+    print(X_test.shape)
+    print(y_pred.shape)
+    print(classifier.score(X_test, y_pred))
     
     new = []
     input_date = datetime.strptime('11/10/2020', '%m/%d/%Y')
-    new.append(input_date.weekday())
-    new.append(0)
+    new.append(input_date.strftime("%W"))
+    new.append(input_date.strftime("%m"))
     
     arr = np.array(new).reshape(1,2)
     scale_arr = scaler.transform(arr)
