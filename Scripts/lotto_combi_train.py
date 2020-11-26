ad122ccdb731c9cd933ef37099a9b7a0341ed705
Jared Dominic V. Caraan <j.caraan04@gmail.com>
2020-11-27 01:20:42 +0800

    decomposed week into two, retrained model

--- Scripts/lotto_combi_train.py
+++ Scripts/lotto_combi_train.py
@@ -4,10 +4,10 @@ import pandas as pd
 import joblib
 
 from scipy import stats
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 from pprint import pprint
 from config import *
 
@@ -25,8 +25,8 @@ logger.addHandler(file_handler)
 def train(df, col, label, scaler_, model):
     
     #Split into dependent and independent variables
-    X = df.iloc[:,0:1].values 
-    y = df.iloc[:,1].values
+    X = df.iloc[:,0:2].values 
+    y = df.iloc[:,2].values
     
     #Train and Test data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, random_state = rand_state)
@@ -53,8 +53,6 @@ def train(df, col, label, scaler_, model):
     #Metrics
     try:
         logger.debug("Accuracy = {0:.2f}".format(accuracy_score(y_test,y_pred) * 100))
-        logger.debug("Precision = {0:.2f}".format(precision_score(y_test,y_pred) * 100))
-        logger.debug("Recall = {0:.2f}".format(recall_score(y_test,y_pred) * 100))
     except Exception as e:
         logger.critical("Exception: " + str(e))
     
@@ -93,38 +91,23 @@ def main():
         
         #Adding features
         df['Date']  = pd.to_datetime(df['Date'], format = '%d/%m/%Y')
-        df['Year']  = df['Date'].dt.year
-        df['Month'] = df['Date'].dt.month
         df['Week']  = df['Date'].dt.isocalendar().week
-        df['Day']   = df['Date'].dt.day
-        df['Dweek'] = df['Date'].dt.dayofweek
         
-        
+        df['week_cos'] = np.cos(2 * np.pi * df['Week'] / 7)
+        df['week_sin'] = np.sin(2 * np.pi * df['Week'] / 7)
         
         #Factorizing categorical value
         logger.debug("Column: " + str(col_list[i]))
-        logger.debug(df.head())
         label_list = sorted(df[col_list[i]].unique())
         
         factor_list = list(np.arange(0,len(label_list)))
         df.loc[:,col_list[i]] = df.loc[:,col_list[i]].replace(label_list, factor_list)
         
-        corr_df = df.corr()[[col_list[i]]].sort_values(by=col_list[i], ascending=False)
-        ind     = corr_df.iloc[[1]].index.to_numpy()[0]
-        
-        print(ind)
-        df = df[[ind, col_list[i]]]
-        
-        print(df.head())
-        # factor_month = pd.factorize(df['Month'])
-        # df.Month     = factor_month[0]
-        
-        # factor_week = pd.factorize(df['Week'])
-        # df.Week     = factor_week[0]
+        df = df[['week_cos', 'week_sin', col_list[i]]]
 
-        # logger.debug(df.head())
+        logger.debug(df.head())
         
-        # train(df, str(col_list[i]), label_list, scaler_num, model_num)
+        train(df, str(col_list[i]), label_list, scaler_num, model_num)
 
 if __name__ == "__main__":
     main()
